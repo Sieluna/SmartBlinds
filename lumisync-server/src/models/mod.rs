ad0cf71a0e94fe727a377148e1231484a3ed9620
Sieluna <seele.peng@gmail.com>
2024-09-05 11:08:16 +0300

    Pump dependencies and clean code

--- lumisync-server/src/models/mod.rs
+++ lumisync-server/src/models/mod.rs
@@ -9,19 +9,9 @@ pub mod user_region;
 pub mod window;
 pub mod window_setting;
 
-pub trait Table: CloneTable {
+pub trait Table {
     fn name(&self) -> &'static str;
     fn create(&self) -> String;
     fn dispose(&self) -> String;
     fn dependencies(&self) -> Vec<&'static str>;
-}
-
-pub trait CloneTable {
-    fn clone_table(&self) -> Box<dyn Table>;
-}
-
-impl<T: Table + Clone + 'static> CloneTable for T {
-    fn clone_table(&self) -> Box<dyn Table> {
-        Box::new(self.clone())
-    }
 }
